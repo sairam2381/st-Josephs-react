f652f5813be1220ff3f7aafc6d4586bd7c6baf61
sairam2381 <sairam2381@gmail.com>
2025-04-02 11:25:54 +0530

    Recent code

---
+++ src/home-page/our-promise/our-promise-data.js
@@ -0,0 +1,52 @@
+import {
+  faTabletScreenButton,
+  faPeopleGroup,
+  faDiamondTurnRight,
+  faLifeRing,
+  faShieldHalved,
+  faLaptopCode,
+} from "@fortawesome/free-solid-svg-icons";
+const OurPromiseData = [
+  {
+    title: "Top Code Quality",
+    description:
+      "Our commitment goes beyond mere code - it encompasses providing solutions. You receive W3C fully compliant markup, without any compromise on quality.",
+    icon: faLaptopCode,
+    color: "#44b2b3",
+  },
+  {
+    title: "Responsive",
+    description:
+      "We understand the value of a positive attitude, timely responsiveness, and adaptability. We're dedicated to attentively listening and leveraging our utmost capabilities to serve you.",
+    icon: faTabletScreenButton,
+    color: "#efaf4b",
+  },
+  {
+    title: "Rockstar Team",
+    description:
+      "Our expertise is our unique selling proposition. You'll have access to an all-star team of experienced professionals fully dedicated to serving you, armed with coding skills that are truly exceptional",
+    icon: faPeopleGroup,
+    color: "#00bdeb",
+  },
+  {
+    title: "Fast Turn-arounds",
+    description:
+      "The importance of speed is on par with quality for us. We are swift, nimble, and capable of providing high-quality code within a timeframe that suits your preferences",
+    icon: faPeopleGroup,
+    color: "#00bdeb",
+  },
+  {
+    title: "Life-time support",
+    description:
+      "Our commitment to code quality is exemplified by our enduring support. If any bugs happen to emerge in the future, feel free to reach out to us for assistance.",
+    icon: faLifeRing,
+    color: "#fc2947",
+  },
+  {
+    title: "Secured Agreement",
+    description:
+      "The creative work you receive is entirely your own, and we unequivocally respect that. We assure you of our commitment and readiness to formalize this with a Non-Disclosure Agreement (NDA).",
+    icon: faShieldHalved,
+    color: "#32c766",
+  },
+];
