24038f2edd04fd0300df572139096062b494c733
sairam2381 <sairam2381@gmail.com>
2025-04-02 11:48:21 +0530

    The latest code

--- src/home-page/our-promise/our-promise-component.jsx
+++ src/home-page/our-promise/our-promise-component.jsx
@@ -11,6 +11,7 @@ import {
   faLaptopCode,
 } from "@fortawesome/free-solid-svg-icons";
 import { OurpromiseStyle } from "./our-promise-style";
+import { OurPromiseData } from "./our-promise-data";
 const OurpromisesComponent = () => {
   return (
     <OurpromiseStyle>
@@ -22,81 +23,21 @@ const OurpromisesComponent = () => {
         </h2>
       </div>
       <div className="element">
-        <div className="element__item child1">
-          <div className="item">
-            <FontAwesomeIcon icon={faLaptopCode} size="2xl" color="#44b2b3" />
-            <div className="item__title">Top code quality</div>
-          </div>
-          <div className="element__content">
-            Our commitment goes beyond mere code - it encompasses providing
-            solutions. You receive W3C fully compliant markup, without any
-            compromise on quality.
-          </div>
-        </div>
-
-        <div className="element__item child2">
-          <div className="item">
-            <FontAwesomeIcon
-              icon={faTabletScreenButton}
-              size="2xl"
-              color="#efaf4b"
-            />
-            <div className="item__title">Responsive</div>
-          </div>
-          <div className="element__content">
-            We understand the value of a positive attitude, timely
-            responsiveness, and adaptability. We're dedicated to attentively
-            listening and leveraging our utmost capabilities to serve you.
-          </div>
-        </div>
-        <div className="element__item child3">
-          <div className="item">
-            <FontAwesomeIcon icon={faPeopleGroup} size="2xl" color="#00bded" />
-            <div className="item__title">Rockstar Team</div>
-          </div>
-          <div className="element__content">
-            Our expertise is our unique selling proposition. You'll have access
-            to an all-star team of experienced professionals fully dedicated to
-            serving you, armed with coding skills that are truly exceptional
-          </div>
-        </div>
-        <div className="element__item child4">
-          <div className="item">
-            <FontAwesomeIcon
-              icon={faDiamondTurnRight}
-              size="2xl"
-              color="#864AF9"
-            />
-            <div className="item__title">Fast Turn-arounds</div>
-          </div>
-          <div className="element__content">
-            The importance of speed is on par with quality for us. We are swift,
-            nimble, and capable of providing high-quality code within a
-            timeframe that suits your preferences.
-          </div>
-        </div>
-        <div className="element__item child5">
-          <div className="item">
-            <FontAwesomeIcon icon={faLifeRing} size="2xl" color="#fc2947" />
-            <div className="item__title">Life-time support</div>
-          </div>
-          <div className="element__content">
-            Our commitment to code quality is exemplified by our enduring
-            support. If any bugs happen to emerge in the future, feel free to
-            reach out to us for assistance.
-          </div>
-        </div>
-        <div className="element__item child6">
-          <div className="item">
-            <FontAwesomeIcon icon={faShieldHalved} size="2xl" color="#32c766" />
-            <div className="item__title">Secured Agreement</div>
-          </div>
-          <div className="element__content">
-            The creative work you receive is entirely your own, and we
-            unequivocally respect that. We assure you of our commitment and
-            readiness to formalize this with a Non-Disclosure Agreement (NDA)
-          </div>
-        </div>
+        {OurPromiseData.map((item, index) => {
+          return (
+            <div className="element__item " style={{ borderTop: item.color }}>
+              <div className="item">
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  size="2xl"
+                  color={item.colorIcon}
+                />
+                <div className="item__title">{item.title}</div>
+              </div>
+              <div className="element__content">{item.description}</div>
+            </div>
+          );
+        })}
       </div>
     </OurpromiseStyle>
   );
--- src/home-page/our-promise/our-promise-data.js
+++ src/home-page/our-promise/our-promise-data.js
@@ -6,47 +6,53 @@ import {
   faShieldHalved,
   faLaptopCode,
 } from "@fortawesome/free-solid-svg-icons";
-const OurPromiseData = [
+export const OurPromiseData = [
   {
     title: "Top Code Quality",
     description:
       "Our commitment goes beyond mere code - it encompasses providing solutions. You receive W3C fully compliant markup, without any compromise on quality.",
     icon: faLaptopCode,
-    color: "#44b2b3",
+    color: "4px solid #44b2b3",
+    colorIcon: " #44b2b3",
   },
   {
     title: "Responsive",
     description:
       "We understand the value of a positive attitude, timely responsiveness, and adaptability. We're dedicated to attentively listening and leveraging our utmost capabilities to serve you.",
     icon: faTabletScreenButton,
-    color: "#efaf4b",
+    color: "4px solid #efaf4b",
+    colorIcon: "#efaf4b",
   },
   {
     title: "Rockstar Team",
     description:
       "Our expertise is our unique selling proposition. You'll have access to an all-star team of experienced professionals fully dedicated to serving you, armed with coding skills that are truly exceptional",
     icon: faPeopleGroup,
-    color: "#00bdeb",
+    color: "4px solid #00bdeb",
+    colorIcon: "#00bdeb",
   },
   {
     title: "Fast Turn-arounds",
     description:
       "The importance of speed is on par with quality for us. We are swift, nimble, and capable of providing high-quality code within a timeframe that suits your preferences",
     icon: faPeopleGroup,
-    color: "#00bdeb",
+    color: "4px solid #00bdeb",
+    colorIcon: " #00bdeb",
   },
   {
     title: "Life-time support",
     description:
       "Our commitment to code quality is exemplified by our enduring support. If any bugs happen to emerge in the future, feel free to reach out to us for assistance.",
     icon: faLifeRing,
-    color: "#fc2947",
+    color: "4px solid #fc2947",
+    colorIcon: "  #fc2947",
   },
   {
     title: "Secured Agreement",
     description:
       "The creative work you receive is entirely your own, and we unequivocally respect that. We assure you of our commitment and readiness to formalize this with a Non-Disclosure Agreement (NDA).",
     icon: faShieldHalved,
-    color: "#32c766",
+    color: "4px solid #32c766",
+    colorIcon: "#32c766",
   },
 ];
