11afb7d3b1226617e89f1fa1ca73a8a0a233bb83
sairam2381 <sairam2381@gmail.com>
2025-04-03 09:22:43 +0530

    Recent code

--- src/home-page/user-page-details/user-page-component.jsx
+++ src/home-page/user-page-details/user-page-component.jsx
@@ -3,11 +3,18 @@ const UserPageComponent = () => {
   const [userDetails, setUserDetails] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      const ans = await response.json();
-      setUserDetails(ans);
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+          //   "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        const ans = await response.json();
+        console.log("Checked:", ans);
+
+        setUserDetails(ans);
+      } catch (e) {
+        console.log("The error is", e);
+      }
     };
     fetchData();
   }, []);
