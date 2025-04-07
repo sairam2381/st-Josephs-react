521fbd414781106429c949065764067909dd184e
sairam2381 <sairam2381@gmail.com>
2025-04-07 09:30:55 +0530

    The code is pushed

--- src/home-page/dummy/dummy-component.jsx
+++ src/home-page/dummy/dummy-component.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 const DummyComponent = () => {
-  return <h1>Hello world</h1>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button
+        onClick={() => {
+          navigate("/quiz/user");
+        }}
+      >
+        User Page
+      </button>
+      <button
+        onClick={() => {
+          navigate("/quiz/hero-page");
+        }}
+      >
+        Hero Page
+      </button>
+    </div>
+  );
 };
 export default DummyComponent;
